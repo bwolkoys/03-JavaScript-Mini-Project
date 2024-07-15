35df15be08e01cda1fa06770f96810a8d83680f3
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-07-15 12:56:51 -0700

    updating

---
+++ script.js
@@ -0,0 +1,16 @@
+//prompt user for r, p, or s
+const choices = [`rock`, `paper`, `scissors`];
+
+const userchoice = prompt("Please pick either rock, paper, or scissors.")
+
+const userChoiceIsValid = choices.includes(userChoice.toLowerCase());
+
+if (userChoiceIsValid) {
+
+} else {
+    startRockPaperScissorGame();
+}
+
+
+
+startRockPaperScissorGame();
