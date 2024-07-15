c93fe12302329625d9bb0806a6345c9c776cab8c
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-07-15 13:57:46 -0700

    work

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 //prompt user for r, p, or s
 const choices = [`rock`, `paper`, `scissors`];
 
-const userchoice = prompt("Please pick either rock, paper, or scissors.")
+//
+function startRockPaperScissorGame() {
+    const userChoice = prompt("Please pick either rock, paper, or scissors.")
+    const userChoiceIsValid = choices.includes(userChoice.toLowerCase());
 
-const userChoiceIsValid = choices.includes(userChoice.toLowerCase());
+    if (userChoiceIsValid) {
+        console.log("Choice is valid")
+        const computerChoice = choices[Math.floor(Math.random() * 3)]
+        console.log(computerChoice)
+        if (userChoice === "rock" && computerChoice === "rock") {
+            alert("It's a tie!")
+        }
+        else if (userChoice === "rock" && computerChoice === "paper") {
+            alert("You lose!")
+        }
+        else if (userChoice === "rock" && computerChoice === "scissors") {
+            alert("You win!")
+        }
+        else if (userChoice === "paper" && computerChoice === "paper") {
+            alert("It's a tie!")
+        }
+        else if (userChoice === "paper" && computerChoice === "rock") {
+            alert("You win!")
+        }
+        else if (userChoice === "paper" && computerChoice === "scissors") {
+            alert("You lose!")
+        }
+        else if (userChoice === "scissors" && computerChoice === "scissors") {
+            alert("It's a tie!")
+        }
+        else if (userChoice === "scissors" && computerChoice === "rock") {
+            alert("You lose!")
+        }
+        else if (userChoice === "scissors" && computerChoice === "paper") {
+            alert("You win!")
+        }
 
-if (userChoiceIsValid) {
 
-} else {
-    startRockPaperScissorGame();
+
+    } else {
+        startRockPaperScissorGame();
+    }
 }
 
 
